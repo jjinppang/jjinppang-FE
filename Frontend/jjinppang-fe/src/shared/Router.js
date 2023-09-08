fcc926d1324088eb24f24d8c1d20f66230290da8
Undong00 <ehddjs85246@gmail.com>
2023-09-08 14:07:50 +0900

    feat : 카카오 맵 api 연결 및 라우터 연결

--- Frontend/jjinppang-fe/src/shared/Router.js
+++ Frontend/jjinppang-fe/src/shared/Router.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from '../pages/Login'
-import Main from '../pages/Main'
-import Signup from '../pages/Signup'
-import EmailLogin from '../pages/EmailLogin'
-import Redirection from '../socialLogin/Redirection'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import Signup from "../pages/Signup";
+import EmailLogin from "../pages/EmailLogin";
+import Redirection from "../socialLogin/Redirection";
+import Map from "../pages/Map";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            {/* <Navbar clientRef={clientRef} /> */}
-            <Routes>
-                <Route path="/main" element={<Main />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/emailLogin" element={<EmailLogin />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login/redirect" element={<Redirection />} />
-            </Routes>
-        </BrowserRouter>
-    )
-
-
+  return (
+    <BrowserRouter>
+      {/* <Navbar clientRef={clientRef} /> */}
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/emailLogin" element={<EmailLogin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login/redirect" element={<Redirection />} />
+        <Route path="/map" element={<Map />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default Router
+export default Router;
